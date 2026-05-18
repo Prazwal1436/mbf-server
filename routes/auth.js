12a947287490e0963888dc3974b24ee41340587a
Prazwal1436 <prazwal.parajuli2000@gmail.com>
2026-05-18 12:59:18 +0545

    djhsjdhw

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const { verifyToken, verifyApiKey } = require('../middleware');
 const { validateInput } = require('../utils/validation');
 const User = require('../models/User');
